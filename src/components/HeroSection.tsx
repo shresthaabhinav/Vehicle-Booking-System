97c0f61af239929c2c59e96a6b6b212ba8de32b3
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-05-19 16:25:18 +0545

    changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,34 @@
+"use client"
+import { Bike, Bus, Car, Truck } from "lucide-react";
+import { motion } from "motion/react";
+
 export default function HeroSection() {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
       <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage:"url('/heroImage.jpg ')"}}/>
+      <div className="absolute inset-0 bg-black/80"/>
+      <div className="relative z-10 min-h-screen flex flex-col items-center justify-center px-4 text-center">
+
+      <motion.div initial={{opacity:0,y:30}} animate={{opacity:1,y:0}} transition={{duration:0.6}} className="text-white font-extrabold text-4xl sm:text-5xl md:text-7xl">
+        Book Any Vehicle
+      </motion.div>
+
+      <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.6}} className="mt-4 max-w-xl text-gray-300">
+        From daily rides to heavy transport - all in one platform.
+      </motion.p>
+
+      <motion.div initial={{ opacity:0 }} animate={{ opacity:1 }} transition={{ delay:0.3 }} className="mt-8 flex gap-8 text-gray-300">
+        <Bike size={30}/>
+        <Car size={30}/>
+        <Bus size={30}/>
+        <Truck size={30}/>
+
+      </motion.div>
+
+      <motion.button whileHover={{ scale }} className="bg-white text-black rounded-full font-semibold shadow-xl">
+        Book Now
+      </motion.button>
+      </div>
     </div>
   )
 }
