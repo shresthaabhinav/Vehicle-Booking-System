06832698635fd7cc8fd3885754a65bd5ba7f9b7a
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-05-24 21:27:27 +0545

    changes

--- src/app/api/auth/verify-email/route.ts
+++ src/app/api/auth/verify-email/route.ts
@@ -1,22 +1,47 @@
 import connectDb from "@/lib/db";
+import User from "@/models/user.model";
 
-export async function POST(req:Request){
-    try{
-        await connectDb()
-        const {email,otp} = await req.json()
-        if (email && !otp) {
-              return Response.json(
-                { message: "email and otp is required" },
-                { status: 400 },
-              );
-            }
-        if(!email && !otp){
-            return Response.json(
-                { message:"email and otp is required" },
-                { status: 400 }
-        )
-        }
-    }catch(error){
-
+export async function POST(req: Request) {
+  try {
+    await connectDb();
+    const { email, otp } = await req.json();
+    if (email && !otp) {
+      return Response.json(
+        { message: "email and otp is required" },
+        { status: 400 },
+      );
     }
+    let user = await User.findOne({ email });
+    if (!user) {
+      return Response.json({ message: "user not found" }, { status: 400 });
+    }
+
+    if (user.isEmailVerified) {
+      return Response.json(
+        { message: "Email is already verified" },
+        { status: 400 },
+      );
+    }
+
+    if (user.otpExpiresAt || user.otpExpiresAt < new Date()) {
+      return Response.json(
+        { message: "otp has been expired" },
+        { status: 400 },
+      );
+    }
+
+    if (!user.otp || user.otp != otp) {
+      return Response.json(
+        { message: "invalid otp" },
+        { status: 400 },
+      );
+    }
+
+    user.isEmailVerified=true
+    user.otp=undefined
+    user.otpExpiresAt=undefined
+
+    await user.save()
+
+  } catch (error) {}
 }
