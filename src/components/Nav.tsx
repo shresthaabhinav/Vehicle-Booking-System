3dbf303401850be7fd116d3f66ca7d60da16771f
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-05-27 16:27:51 +0545

    changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,65 +1,101 @@
-'use client'
-import React, { useState } from 'react'
-import { motion } from 'motion/react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import AuthModal from './AuthModal'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/redux/store'
+"use client";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import AuthModal from "./AuthModal";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { ChevronRight } from "lucide-react";
 
-const Nav_Items=["Home", "Bookings", "About Us", "Contact"]
+const Nav_Items = ["Home", "Bookings", "About Us", "Contact"];
 
 export default function Nav() {
-
-  const pathName = usePathname()
-  const [ authOpen, setAuthOpen ] = useState(false)
-  const { userData } = useSelector((state: RootState)=>state.user)
+  const pathName = usePathname();
+  const [authOpen, setAuthOpen] = useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const { userData } = useSelector((state: RootState) => state.user);
 
   return (
     <>
-    <motion.div initial={{ y: -60, opacity: 0 }}
-    animate={{ y:0, opacity: 1 }}
-    className={`fixed top-3 left-1/2 -translate-x-1/2 w-[94%] md:w-[86%] z-50 rounded-full bg-[#0B0B0B] text-white shadow-[0_15px_50px_rgba(0,0,0,0.7)] py-3`}>
-      <div className='max-w-7xl mx-auto px-4 md:px-8 flex items-center justify-between'>
-        <Image src={"/logo.png"} alt='logo' width={44} height={44} priority/>
+      <motion.div
+        initial={{ y: -60, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        className={`fixed top-3 left-1/2 -translate-x-1/2 w-[94%] md:w-[86%] z-50 rounded-full bg-[#0B0B0B] text-white shadow-[0_15px_50px_rgba(0,0,0,0.7)] py-3`}
+      >
+        <div className="max-w-7xl mx-auto px-4 md:px-8 flex items-center justify-between">
+          <Image src={"/logo.png"} alt="logo" width={44} height={44} priority />
 
-        <div className='hidden md:flex items-center gap-10'>
-        {Nav_Items.map((i,index)=>{
-          let href
-          if(i=="Home"){
-            href = '/'
-          }else{
-            href=`/${i.toLowerCase()}`
-          }
-          
-          const active = href == pathName 
+          <div className="hidden md:flex items-center gap-10">
+            {Nav_Items.map((i, index) => {
+              let href;
+              if (i == "Home") {
+                href = "/";
+              } else {
+                href = `/${i.toLowerCase()}`;
+              }
 
-          return <Link key={index} href={href} className={`text-sm font-medium transition ${active ? "text-white" : "text-gray-400 hover:text-white"}`}>{i}</Link>
-        })}
-      </div>
-      <div className='flex items-center gap-3 relative'>
+              const active = href == pathName;
 
-        <div className='hidden md:block relative'>
-          {!userData ? (
-            <button className='px-4 p-1.5 rounded-full bg-white text-black text-sm' onClick={()=>setAuthOpen(true)}>
-          Login
-        </button>
-          ):(
-            <>
-            <button className='w-11 h-11 rounded-full bg-white text-black font-bold'>
-              {userData.name.charAt(0).toUpperCase()}
-            </button>
-            </>
-          )}
+              return (
+                <Link
+                  key={index}
+                  href={href}
+                  className={`text-sm font-medium transition ${active ? "text-white" : "text-gray-400 hover:text-white"}`}
+                >
+                  {i}
+                </Link>
+              );
+            })}
+          </div>
+          <div className="flex items-center gap-3 relative">
+            <div className="hidden md:block relative">
+              {!userData ? (
+                <button
+                  className="px-4 p-1.5 rounded-full bg-white text-black text-sm"
+                  onClick={() => setAuthOpen(true)}
+                >
+                  Login
+                </button>
+              ) : (
+                <>
+                  <button
+                    className="w-11 h-11 rounded-full bg-white text-black font-bold"
+                    onClick={() => setProfileOpen((p) => !p)}
+                  >
+                    {userData.name.charAt(0).toUpperCase()}
+                  </button>
+
+                  <AnimatePresence>
+                    {profileOpen && (
+                      <motion.div
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        className="absolute top-14 right-0 w-[300px] bg-white text-black rounded-2xl shadow-xl border"
+                      >
+                        <div className="p-5">
+                        <p className="font-semibold text-lg">{userData.name}</p>
+                        <p className="text-xs uppercase text-gray-500 mb-4">{userData.role}</p>
+                        {userData.role!="partner" && 
+                          <div className="w-full flex items-center gap-3 py-3 hover:bg-gray-100 rounded-xl">
+                            Become a Partner
+                            <div>
+                            </div>
+                            <ChevronRight size={16} className="ml-auto"/>
+                          </div>}
+                        </div>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </>
+              )}
+            </div>
+          </div>
         </div>
-        
-      </div>
-
-      </div>
-      
-    </motion.div>
-      <AuthModal open={authOpen} onClose={()=>setAuthOpen(false)}/>
-        </>
-  )
+      </motion.div>
+      <AuthModal open={authOpen} onClose={() => setAuthOpen(false)} />
+    </>
+  );
 }
