922e759268fccc9cd8a7fc5d4b7cb27bfbc5dc14
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-06-03 09:19:52 +0545

    updates

--- src/app/partner/onboarding/documents/page.tsx
+++ src/app/partner/onboarding/documents/page.tsx
@@ -1,10 +1,41 @@
 'use client'
 import React from 'react'
+import { motion } from 'motion/react'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export default function page() {
+  const router = useRouter();
   return (
-    <div>
-      
+    <div className='min-h-screen bg-white flex items-center justify-center px-4'>
+      <motion.div
+        initial={{ opacity: 0, y: 28 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className='w-full max-w-xl bg-white rounded-3xl border border-gray-200 shadow-[0_25px_70px_rgba(0,0,0,0.15)] p-6 sm:p-8'
+      >
+        <div className="relative text-center">
+          <button
+            className="absolute left-0 top-0 w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100 transition"
+            onClick={() => router.back()}
+          >
+            <ArrowLeft size={18} />
+          </button>
+
+          <p className="text-xs text-gray-500 font-medium">Step 2 of 3</p>
+
+          <h1 className="text-2xl font-bold mt-1">Upload Documents</h1>
+
+          <p className="text-sm text-gray-500 mt-2">
+            Required for verification
+          </p>
+        </div>
+
+        <div>
+          
+        </div>
+
+      </motion.div>
     </div>
   )
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -8,7 +8,7 @@ export async function proxy(req: NextRequest) {
   if (
     pathname.startsWith("/_next") ||
     pathname.startsWith("/favicon.ico") ||
-    pathname.startsWith(".")
+    /\.(png|jpg|jpeg|gif|svg|webp|ico)$/i.test(pathname)
   ) {
     return NextResponse.next();
   }
@@ -44,7 +44,7 @@ export async function proxy(req: NextRequest) {
   }
 
   if (pathname.startsWith("/api")) {
-    if (!session.user) {
+    if (!session || !session.user) {
       return Response.json(
         {
           message: "unauthorize",
