77fd435458f4f64434301d88691fcf83b255fb28
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-06-04 09:43:33 +0545

    changes in bank section

--- src/app/partner/onboarding/bank/page.tsx
+++ src/app/partner/onboarding/bank/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 import React from 'react'
 import { motion } from 'motion/react'
+import { ArrowLeft, BadgeCheck } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export default function page() {
+
+  const router = useRouter()
   return (
     <div className='min-h-screen bg-white flex items-center justify-center px-4'>
       <motion.div
@@ -10,6 +14,33 @@ export default function page() {
         animate={{ opacity: 1, y: 0 }}
         className='w-full max-w-xl bg-white rounded-3xl border bordr-gray-200 shadow-[0_25px_70px_rgba(0,0,0,0.15)] p-6 sm:p-8'
       >
+        <div className="relative text-center">
+          <button
+            className="absolute left-0 top-0 w-9 h-9 rounded-full border border-gray-300 flex items-center justify-center hover:bg-gray-100 transition"
+            onClick={() => router.back()}
+          >
+            <ArrowLeft size={18} />
+          </button>
+
+          <p className="text-xs text-gray-500 font-medium">Step 3 of 3</p>
+
+          <h1 className="text-2xl font-bold mt-1">Bank and Payout Setup</h1>
+
+          <p className="text-sm text-gray-500 mt-2">
+            Used for partner payouts
+          </p>
+        </div>
+
+        <div className='mt-8 space-y-6'>
+          <div className=''>
+            <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>Account holder name</label>
+
+            <div className='flex items-center gap-2 mt-2'>
+              <div className='text-gray-400'><BadgeCheck/></div>
+              <input type="text" placeholder='As per bank records' className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'/>
+            </div>
+          </div>
+        </div>
 
       </motion.div>
     </div>
