a8256611abd9f7749227a16cc6e5e1a26744e783
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-06-04 11:04:10 +0545

    changes

--- src/app/partner/onboarding/bank/page.tsx
+++ src/app/partner/onboarding/bank/page.tsx
@@ -32,7 +32,34 @@ export default function page() {
         </div>
 
         <div className='mt-8 space-y-6'>
-          <div className=''>
+          <div>
+            <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>Account holder name</label>
+
+            <div className='flex items-center gap-2 mt-2'>
+              <div className='text-gray-400'><BadgeCheck/></div>
+              <input type="text" placeholder='As per bank records' className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'/>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>Account holder name</label>
+
+            <div className='flex items-center gap-2 mt-2'>
+              <div className='text-gray-400'><BadgeCheck/></div>
+              <input type="text" placeholder='As per bank records' className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'/>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>Account holder name</label>
+
+            <div className='flex items-center gap-2 mt-2'>
+              <div className='text-gray-400'><BadgeCheck/></div>
+              <input type="text" placeholder='As per bank records' className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'/>
+            </div>
+          </div>
+
+          <div>
             <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>Account holder name</label>
 
             <div className='flex items-center gap-2 mt-2'>
