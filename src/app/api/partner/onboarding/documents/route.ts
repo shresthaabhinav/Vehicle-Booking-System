00bbdc8782c935860ca6b876faf9f7a41e07b6cb
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-06-08 08:27:49 +0545

    changes

--- src/app/api/partner/onboarding/documents/route.ts
+++ src/app/api/partner/onboarding/documents/route.ts
@@ -1,7 +1,9 @@
 import { auth } from "@/auth";
 import uploadOnCloudinary from "@/lib/cloudinary";
 import connectDb from "@/lib/db";
+import PartnerDocs from "@/models/partnerDocs.model";
 import User from "@/models/user.model";
+import Vehicle from "@/models/vehicle.model";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest){
@@ -49,7 +51,44 @@ export async function POST(req: NextRequest){
             }
             updatePayload.citizenshipUrl = url
         }
-    } catch(error){
+        if(license){
+            const url = await uploadOnCloudinary(license)
+            if(!url){
+                return Response.json({ messag: "Citizenship documents upload failed"}
+                ,{ status: 500 }
+            )
+            }
+            updatePayload.licenseUrl = url
+        }
+        if(rc){
+            const url = await uploadOnCloudinary(rc)
+            if(!url){
+                return Response.json({ messag: "Citizenship documents upload failed"}
+                ,{ status: 500 }
+            )
+            }
+            updatePayload.rcUrl = url
+        }
 
+        const partnerDocs = await PartnerDocs.findOneAndUpdate(
+            {owner: user._id},
+            {$set:updatePayload},
+            {upsert:true, new: true}
+        )
+
+        if(user.partnerOnBoardingSteps<2){
+            user.partnerOnBoardingSteps = 2
+        }
+
+        await user.save()
+
+        return Response.json(
+                partnerDocs, { status: 201 }
+            )
+
+    } catch(error){
+        return Response.json({ message: `partner docs error ${error}`}
+                ,{ status: 201 }
+            )
     }
 }
