6305f7b46a52d35824683dbb1ad7545b220285d2
Abhinav <shresthaabhinav5@gmail.com>
2026-07-07 13:03:19 +0545

    changes

--- src/components/SearchMap.tsx
+++ src/components/SearchMap.tsx
@@ -1,5 +1,6 @@
 'use client'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 
 type props={
@@ -10,12 +11,40 @@ type props={
 }
 
 export default function SearchMap({pickUp, drop, onChange, onDistance}:props) {
+
+  const [p1, setP1] = useState<[number,number]>()
+  const [p2, setP2] = useState<[number,number]>()
+
+  const geoCoding = async (q: string):Promise<[number, number] | null> =>{
+    try {
+      const {data} = await axios.get(`https://photon.komoot.io/api/?q=${encodeURIComponent(q)}&limit=1`)
+      if(!data.features.length) return null
+      const [lon, lat] = data.features[0].geometry.coordinates
+      return [lat, lon]
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+
+  useEffect(()=>{
+    if(pickUp && drop){
+      
+      const a = geoCoding(pickUp)
+      const b = geoCoding(drop)
+      if(!a || !b){
+        return
+      }
+      setP1(a)
+      setP2(b)
+    }
+  },[pickUp, drop])
+
   return (
     <div className='relative h-full w-full bg-zinc-100'>
-      <MapContainer
-        style={{ width: "100%", height: "100%" }}
-      />
-      <TileLayer attribution='&copy;'/>
+      <MapContainer style={{ width: "100%", height: "100%" }} center={[0,0]} zoom={13}>
+      <TileLayer attribution='&copy; <a href="https://carto.com/">"CARTO"</a> contributors' url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"/>
+      </MapContainer>
     </div>
   )
 }
