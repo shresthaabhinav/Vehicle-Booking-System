e6691ecb9da577227e027b4d404e7a371cb17e3e
Abhinav <shresthaabhinav5@gmail.com>
2026-07-20 15:29:23 +0545

    changes

--- Gadi/src/lib/socket.ts
+++ Gadi/src/lib/socket.ts
@@ -1,4 +1,9 @@
-let socket: Socket = null
-export const getSocket = () => {
+import { io, Socket } from "socket.io-client"
 
+let socket: Socket | null = null
+export const getSocket = () => {
+    if(!socket){
+        socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL);
+    }
+    return socket
 }
